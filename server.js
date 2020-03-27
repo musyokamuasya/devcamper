f0769656583beb9e9dd666bf51834af735548163
Musyoka Muasya <josephmuasya8@gmail.com>
2020-03-27 22:33:06 +0300

    End of setup for Routes basics

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+// const logger = require('./middleware/logger');
 
 dotenv.config({path:'./config/config.env'});
 const app = express();
 const bootcamps = require('./routes/bootcamps');
+
+// Logging using morgan
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
 app.use('/api/v1/bootcamps', bootcamps);
+
 
 
 
