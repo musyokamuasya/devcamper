cff3864ffc8f30d3570adbbe2eba42c811bbdcf6
Musyoka Muasya <josephmuasya8@gmail.com>
2020-03-29 23:42:47 +0300

    Advanced filtering for lte, lt, gte, gt & in

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,8 +8,14 @@ const Bootcamp = require('../models/Bootcamp');
 // @access  Public
 
 exports.getBootcamps = asyncHandler( async (req, res, next)=>{
+
+    let query;
+    let queryStr = JSON.stringify(req.query);
+    console.log(queryStr);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match =>`$${match}`);
+    query = Bootcamp.find(JSON.parse(queryStr));
     
-        const bootcamps = await Bootcamp.find();
+        const bootcamps = await query;
         res.status(200).json({success: true, count: bootcamps.length, data: bootcamps});
         
 
