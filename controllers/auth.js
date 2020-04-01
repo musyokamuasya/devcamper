fd4d55857d6118b9f749129550752a0850f36045
Musyoka Muasya <josephmuasya8@gmail.com>
2020-04-01 17:26:55 +0300

    User login using token

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc    Register user
-// @route   GET api/v1/auth/register
+// @route   POST api/v1/auth/register
 // @access  Public
 
 exports.register = asyncHandler(async (req, res, next)=>{
@@ -20,3 +20,33 @@ exports.register = asyncHandler(async (req, res, next)=>{
 
         res.status(200).json({ success: true , token});
 });
+
+// @desc    Login in user
+// @route   POST api/v1/auth/login
+// @access  Public
+
+exports.register = asyncHandler(async (req, res, next)=>{
+        const  { email, password } = req.body;
+
+        if (!(email || password)) {
+                next (new ErrorResponse(`Please provide an email and password`, 400));     
+        }
+
+        const user = await User.findOne({ email }).select('+password');
+        console.log(user);
+
+        if (!user) {
+                next (new ErrorResponse(`Authentication error: Email is not registered`, 401));     
+        }
+        // Check if the passwords match
+        const isMatch = await user.matchPasswords(password);
+
+        if (!isMatch) {
+                next (new ErrorResponse(`Authentication error: Enter correct email and password`, 401));    
+        }
+        const token = user.getSignedJwtToken();
+
+        
+  
+          res.status(200).json({ success: true , token});
+  });
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const {register} = require('../controllers/auth');
+const {register, login} = require('../controllers/auth');
 const router = express.Router();
 router.post('/register', register);
+router.post('/login', register);
 
 module.exports = router;
